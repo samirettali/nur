95730c86daff2048f39ebd345bb731b51359f8d5
Samir Ettali <s.ettali@young.business>
2025-07-14 01:53:11 +0200

    opencode: 0.2.6 -> 0.2.33

--- pkgs/opencode/default.nix
+++ pkgs/opencode/default.nix
@@ -6,7 +6,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "0.2.6";
+  version = "0.2.33";
 
   src =
     finalAttrs.passthru.sources.${stdenvNoCC.hostPlatform.system}
@@ -34,11 +34,11 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     sources = {
       "aarch64-darwin" = fetchurl {
         url = "https://github.com/sst/opencode/releases/download/v${finalAttrs.version}/opencode-darwin-arm64.zip";
-        hash = "sha256-ZH52rZu3zGpm4mHE/BnuMuV+FV9qGfZWO+L5S7ABHqg=";
+        hash = "sha256-GwEsjFSN4iahHnQKnA8KQ/YO1sxkzxRL8WrMLYFqG3I=";
       };
       "aarch64-linux" = fetchurl {
         url = "https://github.com/sst/opencode/releases/download/v${finalAttrs.version}/opencode-linux-arm64.zip";
-        hash = "sha256-QQEPu5KYSphOuC9pJydkOzZlYv/BlJwDGnJNGlqSPIk=";
+        hash = "sha256-zGUUO/QFl0EP30ubulDCHV+LNu9ePkwf4ZY2yXdDU+8=";
       };
       # "x86_64-darwin" = fetchurl {
       #   url = "https://github.com/sst/opencode/releases/download/v${finalAttrs.version}/opencode-darwin-x64.zip";
@@ -46,7 +46,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
       # };
       "x86_64-linux" = fetchurl {
         url = "https://github.com/sst/opencode/releases/download/v${finalAttrs.version}/opencode-linux-x64.zip";
-        hash = "sha256-nEPQweFgQ92Ubg2mHiGE1aJlaQl+gtEjFVyzD0t0Hd4=";
+        hash = "sha256-Sd7ew0CrVj2YL0cuhT3hzRIwuIGp47TKamd8qLU29r0=";
       };
     };
 
