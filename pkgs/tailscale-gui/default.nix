a84e3e16f47db5f7abde2cc22e82d2bd80ce7eb0
Samir <ettali.samir@gmail.com>
2026-06-11 17:34:05 +0200

    tailscale-gui: 1.96.5 -> 1.98.5 (#56)
    Automated package update.
    
    Version:
    - old: 1.96.5
    - new: 1.98.5
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- pkgs/tailscale-gui/default.nix
+++ pkgs/tailscale-gui/default.nix
@@ -8,11 +8,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "tailscale-gui";
-  version = "1.96.5";
+  version = "1.98.5";
 
   src = fetchurl {
     url = "https://pkgs.tailscale.com/stable/Tailscale-${finalAttrs.version}-macos.pkg";
-    hash = "sha256-eqwNX5uBGOiy1z91eMMkE6892ot+eXlM5/6jHm9uF8g=";
+    hash = "sha256-r7e8aKNWaX1psI0a3sohTUv8xmUv8oebH/ndjeHLoVA=";
   };
 
   strictDeps = true;
