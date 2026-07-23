e3a024d2ea88247298dd10304b3518f839c854dd
Samir Ettali <samir@ettali.com>
2026-07-23 18:35:16 +0200

    spotctl: 0.6.0 -> 0.7.0

--- pkgs/spotctl/default.nix
+++ pkgs/spotctl/default.nix
@@ -5,13 +5,13 @@
 }:
 buildGoModule rec {
   pname = "spotctl";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "samirettali";
     repo = "spotctl";
     rev = "v${version}";
-    hash = "sha256-eE7Y9anYKmS0TKDyc2W8JPy4/+iP1yVOTkDx4U13Lho=";
+    hash = "sha256-davUPoaXpT4uhR/cZLxzvmGQ/IWNcHqM6mK8YeZdRxc=";
   };
 
   vendorHash = "sha256-ZG+eQhOHW5J1WLm2WZ57ywXA+NobgMorRJkR2Mkb2fY=";
